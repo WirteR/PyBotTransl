d8e7ffd2bdfc4627d958f87b3b309dc1cd0f0bcf
WirteR <kravetsillis16@gmail.com>
2019-01-24 20:42:40 +0200

    new files added

--- PyBot.py
+++ PyBot.py
@@ -4,39 +4,32 @@ from telegram.ext import MessageHandler, Filters
 from googletrans import Translator
 import logging
 
-tok = '754612549:AAEewg5s00ru_8QdPAt2GvpFH4GsaU7cSL0'
+import botfuncs as funcs
+
+tok = '731565223:AAFMQr3wREQFrhU6Zd0k3LLJkWdhCbyZh_8'
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Цей бот призначений для швидкого перекладу ваших слів на англійську. "
-                                                          "Щоб дізнатись більше - введіть /help Приємного користування!")
+if __name__ == "__main__":
+    translator = Translator()
+    bot = telegram.Bot(token = tok)
+    updater = Updater(token = tok)
+    disp = updater.dispatcher
 
-def helper(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Бот для перекладу на англійську. Developed by Kravets')
-
-def echo(bot, update):
-    returning = str(update.message.text)
-    result = translator.translate(returning)
-    bot.send_message(chat_id=update.message.chat_id, text=result.text)
-
-
-translator = Translator()
-bot = telegram.Bot(token = tok)
-updater = Updater(token = tok)
-disp = updater.dispatcher
-
-start_handler = CommandHandler('start', start)
-helper_handler = CommandHandler('help', helper)
-echo_handler = MessageHandler(Filters.text, echo)
-
-disp.add_handler(echo_handler)
-disp.add_handler(start_handler)
-disp.add_handler(helper_handler)
+    start_handler = CommandHandler('start', funcs.start)
+    helper_handler = CommandHandler('help', funcs.helper)
+    settings_handler = CommandHandler('settings', funcs.settings)
+    ch_handler = MessageHandler(Filters.text, funcs.choose)
+    unknown_handler = MessageHandler(Filters.command, funcs.unknown)
 
 
+    disp.add_handler(ch_handler)
+    disp.add_handler(start_handler)
+    disp.add_handler(helper_handler)
+    disp.add_handler(settings_handler)
+    disp.add_handler(unknown_handler)
 
-updater.start_polling()
+    updater.start_polling()
 
 
 
---
+++ botfuncs.py
@@ -0,0 +1,50 @@
+import telegram
+from telegram.ext import  Updater, CommandHandler
+from telegram.ext import MessageHandler, Filters
+from googletrans import Translator
+import logging
+
+tok = '731565223:AAFMQr3wREQFrhU6Zd0k3LLJkWdhCbyZh_8'
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+res = 'en'
+
+translator = Translator()
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Цей бот призначений для швидкого перекладу ваших слів на англійську. "
+                                                          "Щоб дізнатись більше - введіть /help Приємного користування!")
+
+def helper(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Бот для перекладу на англійську. Developed by Kravets')
+
+def settings(bot, update):
+    custom_keyboard = [['Мова для перекладу']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id = update.message.chat_id, text="Виберіть потрібне налаштування:", reply_markup=reply_markup)
+
+def choose(bot, update):
+    global res
+    if update.message.text == 'Мова для перекладу':
+        custom_keyboard = [['English'],['Deutech'],['France']]
+        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+        bot.send_message(chat_id = update.message.chat_id, text="Виберіть потрібну мову:", reply_markup=reply_markup)
+
+    elif update.message.text == 'English':
+            bot.send_message(chat_id = update.message.chat_id, text="Мову змінено на англійську!")
+            res = 'en'
+
+    elif update.message.text == 'Deutech':
+            bot.send_message(chat_id = update.message.chat_id, text="Мову змінено на німецьку!")
+            res = 'de'
+    elif update.message.text == 'France':
+            bot.send_message(chat_id = update.message.chat_id, text="Мову змінено на французьку!")
+            res = 'fr'
+    else:
+        returning = str(update.message.text)
+        result = translator.translate(returning, dest = res)
+        bot.send_message(chat_id=update.message.chat_id, text=result.text)
+
+
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text = "Вибачте, команди не існує")
